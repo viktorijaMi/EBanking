db28a4f310a351728547881797d6e03574c71b6e
viktorijaMi <mijalchevaviktorija@gmail.com>
2021-02-21 20:51:41 +0100

    Added the SSL certificates

--- e-banking app/src/main/java/com/ebanking/ebanking/config/WebSecurityConfig.java
+++ e-banking app/src/main/java/com/ebanking/ebanking/config/WebSecurityConfig.java
@@ -1,11 +1,18 @@
 package com.ebanking.ebanking.config;
 
+import com.ebanking.ebanking.service.UserService;
+import com.ebanking.ebanking.service.impl.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -15,35 +22,59 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
     private final CustomUsernameAndPasswordProvider customUsernameAndPasswordProvider;
-
-    public WebSecurityConfig(PasswordEncoder passwordEncoder, CustomUsernameAndPasswordProvider customUsernameAndPasswordProvider) {
+    private final UserService userService;
+    public WebSecurityConfig(PasswordEncoder passwordEncoder, CustomUsernameAndPasswordProvider customUsernameAndPasswordProvider, UserService userService) {
         this.passwordEncoder = passwordEncoder;
         this.customUsernameAndPasswordProvider = customUsernameAndPasswordProvider;
+        this.userService = userService;
     }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/register").permitAll()
+//                .antMatchers("/home/add/**").hasRole("ADMIN")
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login").permitAll()
+//                .failureUrl("/login?error=BadCredentials")
+//                .defaultSuccessUrl("/home", true)
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .clearAuthentication(true)
+//                .invalidateHttpSession(true)
+//                .deleteCookies("JSESSIONID");
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(customUsernameAndPasswordProvider);
+//    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/register").permitAll()
-                .antMatchers("/home/add/**").hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
+        http.authorizeRequests().anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .failureUrl("/login?error=BadCredentials")
-                .defaultSuccessUrl("/home", true)
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID");
+                .x509()
+                .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+                .userDetailsService(userDetailsService());
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(customUsernameAndPasswordProvider);
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                if (username.equals("viktorija")) {
+                    return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+                }
+                throw new UsernameNotFoundException(username);
+            }
+        };
     }
 }
--- e-banking app/src/main/java/com/ebanking/ebanking/web/HomeController.java
+++ e-banking app/src/main/java/com/ebanking/ebanking/web/HomeController.java
@@ -4,11 +4,13 @@ import com.ebanking.ebanking.model.PrimaryAccount;
 import com.ebanking.ebanking.model.User;
 import com.ebanking.ebanking.service.PrimaryAccountService;
 import com.ebanking.ebanking.service.UserService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -23,15 +25,17 @@ public class HomeController {
         this.userService = userService;
     }
 
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     @GetMapping
     public String getHomePage(@RequestParam(required = false) String message,
                               Model model,
-                              HttpServletRequest request) {
+                              HttpServletRequest request,
+                              Principal principal) {
         if (message != null && !message.isEmpty()) {
             model.addAttribute("hasMessage", true);
             model.addAttribute("message", "TRANSFER SUCCESSFUL");
         }
-        User user = userService.findByUsername(request.getRemoteUser()).get();
+        User user = userService.findByUsername(principal.getName()).get();
         model.addAttribute("user", user);
         model.addAttribute("bodyContent", "home");
         return "master-template";
